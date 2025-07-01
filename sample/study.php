c9d9b70726d1fc363cf7ea263fb6efa52b4cc6cb
302A-01\cre <a>
2025-07-01 12:19:41 +0900

    q8から

--- sample/study.php
+++ sample/study.php
@@ -2,3 +2,20 @@
 // このファイルはphp study.phpで実行できます
 // これはコメントアウトです
 // コメントアウトはプログラムとして認識されず自由に書くことができます。
+echo "Hello World！\n";
+$var1 = "これは変数です";
+echo $var1;
+echo "\n";
+echo $var1;
+$var2 = "同じ変数を何度でも使用できます\n";
+echo $var2;
+echo $var2;
+echo $var2;
+
+$var2 = "変数は上書きが可能です\n";
+echo $var2;
+
+$str1 = '文字列1';
+$str1 .= '文字列2';
+echo $str1;
+echo "\n";
